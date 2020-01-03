021acd28504cfaae6db5afbbcc5ae29a65c589fc
karimmfadel <karimmm.fadel@gmail.com>
2020-01-03 07:28:32 +0200

    creating schema for clients data and using NoOpPasswordEncoder in encode the password

--- oauthServer/src/main/java/com/myCompany/oauth/oauthServer/UserConfiguration.java
+++ oauthServer/src/main/java/com/myCompany/oauth/oauthServer/UserConfiguration.java
@@ -1,21 +1,40 @@
 package com.myCompany.oauth.oauthServer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class UserConfiguration extends GlobalAuthenticationConfigurerAdapter {
 
-	PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//	@Autowired
+//	PasswordEncoder passwordEncoder;// = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//	
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//	}
+	
+	@Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+	
+//	@Bean  // {bcrypt}
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 	
 	@Override
 	public void init(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("mohamed").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN")
+		auth.inMemoryAuthentication().withUser("mohamed").password("1234").roles("USER", "ADMIN")
 				.authorities("CAN_READ", "CAN_WRITE")//; //, "CAN_DELETE"
-				.and().withUser("ahmed").password(passwordEncoder.encode("1234"))
+				.and().withUser("ahmed").password("1234")
 				.roles("USER").authorities("CAN_READ");
 	}
 
