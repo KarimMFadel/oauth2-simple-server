c45f40aad5efc164e354e33edd59f68d92889b6c
karimmfadel <karimmm.fadel@gmail.com>
2020-01-04 14:53:29 +0200

    changing encode to use BCryptPasswordEncoder

--- oauthServer/src/main/java/com/myCompany/oauth/oauthServer/UserConfiguration.java
+++ oauthServer/src/main/java/com/myCompany/oauth/oauthServer/UserConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,29 +13,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class UserConfiguration extends GlobalAuthenticationConfigurerAdapter {
 
-//	@Autowired
-//	PasswordEncoder passwordEncoder;// = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	@Autowired
+	PasswordEncoder passwordEncoder;// = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 //	
 //	@Bean
 //	public PasswordEncoder passwordEncoder() {
 //		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 //	}
 	
-	@Bean
-    public static NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-    }
+//	@Bean
+//    public static NoOpPasswordEncoder passwordEncoder() {
+//        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+//    }
 	
-//	@Bean  // {bcrypt}
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
+	@Bean  // {bcrypt}
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	@Override
 	public void init(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("mohamed").password("1234").roles("USER", "ADMIN")
-				.authorities("CAN_READ", "CAN_WRITE")//; //, "CAN_DELETE"
-				.and().withUser("ahmed").password("1234")
+		auth.inMemoryAuthentication().withUser("mohamed").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN")
+				.authorities("CAN_READ", "CAN_WRITE")
+				.and().withUser("ahmed").password(passwordEncoder.encode("1234"))
 				.roles("USER").authorities("CAN_READ");
 	}
 
